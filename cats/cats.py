9c95e08d67dea82548a1dac9d33015ae77071040
Pengshu Yin <fengyuetaoyi@gmail.com>
2024-06-29 11:02:26 +0800

    complete class question 1

--- cats/cats.py
+++ cats/cats.py
@@ -31,6 +31,12 @@ def pick(paragraphs, select, k):
     """
     # BEGIN PROBLEM 1
     "*** YOUR CODE HERE ***"
+    for str in paragraphs:
+        if select(str):
+            k -= 1
+        if k == -1:
+            return str
+    return ''
     # END PROBLEM 1
 
 
@@ -50,6 +56,14 @@ def about(subject):
     assert all([lower(x) == x for x in subject]), 'subjects should be lowercase.'
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    def selectFunction(paragraphs):
+        for words in subject:
+            if words.lower() in remove_punctuation(paragraphs.lower()).split():
+                return True
+        return False
+
+    return selectFunction
+
     # END PROBLEM 2
 
 
@@ -80,6 +94,15 @@ def accuracy(typed, source):
     source_words = split(source)
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    if len(typed_words) == 0 and len(source_words) != 0:
+        return 0.0
+    if len(typed_words) == 0 and len(source_words) == 0:
+        return 100.0
+    correctNum = 0
+    for i in range(min(len(typed_words), len(source_words))):
+        if typed_words[i] == source_words[i]:
+            correctNum += 1
+    return correctNum * 100 / len(typed_words)
     # END PROBLEM 3
 
 
@@ -98,6 +121,7 @@ def wpm(typed, elapsed):
     assert elapsed > 0, 'Elapsed time must be positive'
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    return len(typed) / 5 / elapsed * 60
     # END PROBLEM 4
 
 
